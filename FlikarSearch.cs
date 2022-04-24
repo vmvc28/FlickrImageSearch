59235a9cb0e6f81025f90c8428d692c54bcc7b4e
VMV <chithritha.vmv@siemens-healthineers.com>
2022-04-24 17:07:21 +0530

    initial commit

---
+++ FlikarSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace SearchPictures
+{
+    internal class FlikarSearch
+    {
+        public string PhotosId { get; set; }
+        public string ImgTitle { get; set; }
+        public string Scrt { get; set; }
+        public string FarmeId { get; set; }
+        public string ServerId { get; set; }
+
+        public Uri ImgUrl => new Uri($"http://farm{FarmeId}.static.flickr.com/{ServerId}/{PhotosId}_{Scrt}.jpg");
+    }
+}
---
+++ SearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media.Imaging;
+
+
+namespace SearchPictures
+{
+    class SearchViewModel
+    {
+        private BitmapImage imageData;
+
+        public BitmapImage ImageData
+        {
+            get { return this.imageData; }
+            set { this.imageData = value; }
+        }
+    }
+}
